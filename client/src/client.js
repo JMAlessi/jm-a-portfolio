ef2d54afb5b71ac7e2024abca7870c209d8173cd
J. A. Software <46306007+J-A-Software@users.noreply.github.com>
2023-04-22 21:56:13 -0400

    add projectId and placeholder for token

--- client/src/client.js
+++ client/src/client.js
@@ -1,12 +1,15 @@
 import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
+const projectId = '7rugpmsh';
+const token = '';
+
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-  dataset: 'production',
-  apiVersion: '2022-02-01',
-  useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+	projectId,
+	dataset: 'production',
+	apiVersion: '2022-02-01',
+	useCdn: true,
+	token,
 });
 
 const builder = imageUrlBuilder(client);
